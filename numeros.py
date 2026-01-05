2b88d33d8073f5324e75aa0143b146a1ff1b7cba
Javier Solares <javiersolares@MacBook-Pro-de-Javier.local>
2026-01-05 16:27:43 -0500

    numeros commit

---
+++ numeros.py
@@ -0,0 +1,26 @@
+x = 1
+y = 2.9
+z = 5j
+
+print(type(x),type(y),type(z))
+
+positivo = 1
+negativo = -1
+decimal = -1.1
+complejo = -5j
+imaginario = 5+5j
+
+imaginario = -5- 5j
+
+print(type(positivo),type(negativo),type(decimal),type(complejo))
+
+print(positivo,negativo,decimal,complejo)
+
+
+xf = float(x)
+print(type(xf))
+print(xf)
+
+ye = int(y)
+print(type(ye))
+print(ye)

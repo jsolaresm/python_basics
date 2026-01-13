d746b1254f75c22977cf4b1731259b322632ebea
Javier Solares <javiersolares@MacBook-Pro-de-Javier.local>
2026-01-13 17:46:15 -0500

    textos commit

--- texto.py
+++ texto.py
@@ -29,3 +29,22 @@ limpieza = espacios.strip()
 print(limpieza)
 
 
+texto = "Este es un texto"
+print(texto[0]) #posicion de indice desde 0 en string
+
+print(texto[0:4])
+
+print(texto[0:len(texto)])
+
+
+print(texto[:len(texto)])
+
+texto2 = "este curso es de python"
+print(texto2.replace("python","javascript"))
+textoDividido = texto.split(" ")
+print(textoDividido)
+
+
+texto3 = "Este texto tiene MAYUSCULAS y minusculas y necesito encontrar ciertas palabras"
+
+print("mayusculas".lower() in texto3.lower())

006db7a666d117696ea1cb1380ee66c4b392ec59
Javier Solares <javiersolares@MacBook-Pro-de-Javier.local>
2026-01-13 17:34:57 -0500

    numeros commit

---
+++ texto.py
@@ -0,0 +1,31 @@
+print("Hola mundo")
+print('Hola "mundo"')
+
+ingles = "Hi i'm javier"
+print(ingles)
+multiple = """ Como
+estan?  """
+
+print (multiple)
+
+palabra = "murcielago"
+print(len(palabra))
+palabra = "ejercicios de Python"
+incluida = "Python" in  palabra
+print(incluida)
+
+noincluida = "javascript" not in palabra
+
+print(noincluida)
+
+mayuscula = palabra.upper()
+print(mayuscula)
+
+minuscula = palabra.lower()
+print(minuscula)
+
+espacios = "   este es el texto    "
+limpieza = espacios.strip()
+print(limpieza)
+
+
